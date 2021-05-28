3d13d57fc4abb7039ae767f7fe76efa81fcac9c3
Ben <mail1@sonadresse.com>
2021-05-28 09:25:45 +0200

    refresh addon icon when 2FA is a success

--- src/popup/accounts/two-factor.component.ts
+++ src/popup/accounts/two-factor.component.ts
@@ -15,6 +15,7 @@ import { ApiService } from 'jslib/abstractions/api.service';
 import { AuthService } from 'jslib/abstractions/auth.service';
 import { EnvironmentService } from 'jslib/abstractions/environment.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
+import { MessagingService } from 'jslib/abstractions/messaging.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
 import { StateService } from 'jslib/abstractions/state.service';
 import { StorageService } from 'jslib/abstractions/storage.service';
@@ -25,6 +26,8 @@ import { BroadcasterService } from 'jslib/angular/services/broadcaster.service';
 import { TwoFactorComponent as BaseTwoFactorComponent } from 'jslib/angular/components/two-factor.component';
 
 import { PopupUtilsService } from '../services/popup-utils.service';
+
+import { UserService } from 'jslib/abstractions/user.service';
 
 const BroadcasterSubscriptionId = 'TwoFactorComponent';
 
@@ -41,7 +44,8 @@ export class TwoFactorComponent extends BaseTwoFactorComponent {
         environmentService: EnvironmentService, private ngZone: NgZone,
         private broadcasterService: BroadcasterService, private changeDetectorRef: ChangeDetectorRef,
         private popupUtilsService: PopupUtilsService, stateService: StateService,
-        storageService: StorageService, route: ActivatedRoute) {
+        storageService: StorageService, route: ActivatedRoute,
+        private userService: UserService, protected messagingService: MessagingService) {
         super(authService, router, i18nService, apiService, platformUtilsService, window, environmentService,
             stateService, storageService, route);
         super.onSuccessfulLogin = () => {
@@ -89,5 +93,15 @@ export class TwoFactorComponent extends BaseTwoFactorComponent {
 
     anotherMethod() {
         this.router.navigate(['2fa-options']);
+    }
+
+    async submit() {
+        await super.submit();
+        const isAuthed = await this.userService.isAuthenticated();
+        if (!isAuthed) {
+            // TODO BJA : prompt a message in case 2FA failed, explaining a new code is sent.
+        } else {
+            this.messagingService.send('loggedIn');
+        }
     }
 }
