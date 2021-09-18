043ae8d8aa1d3504fe0b16f2549adecf5e53ebb3
Yannick Chiron <yannick.chiron@gmail.com>
2021-09-18 20:02:14 +0200

    feat: sync created cipher from new organization
    When a new organization (folder) is created, no realtime message is
    sent to other clients
    
    So when a cipher is created inside this new organization, then other
    clients should synchronize this organization before retrieving the new
    cipher
    
    This will allow to get display the new organization in the sidebar but
    also to decrypt the received cipher
    
    Mirrored from cozy/cozy-pass-web@1122e84ae8b744abd2f27ec51ebb0dd22056976b

--- src/services/browserCrypto.service.ts
+++ src/services/browserCrypto.service.ts
@@ -1,7 +1,43 @@
+import { CryptoFunctionService } from 'jslib-common/abstractions/cryptoFunction.service';
 import { KeySuffixOptions } from 'jslib-common/abstractions/storage.service';
+import { LogService } from 'jslib-common/abstractions/log.service';
+import { PlatformUtilsService } from 'jslib-common/abstractions/platformUtils.service';
+import { StorageService } from 'jslib-common/abstractions/storage.service';
+
 import { CryptoService } from 'jslib-common/services/crypto.service';
 
+import { ProfileOrganizationResponse } from 'jslib-common/models/response/profileOrganizationResponse';
+
+const Keys = {
+    encOrgKeys: 'encOrgKeys',
+};
+
 export class BrowserCryptoService extends CryptoService {
+    constructor(
+        private localStorageService: StorageService,
+        secureStorageService: StorageService,
+        cryptoFunctionService: CryptoFunctionService,
+        platformUtilService: PlatformUtilsService,
+        logService: LogService
+    ) {
+        super(
+            localStorageService,
+            secureStorageService,
+            cryptoFunctionService,
+            platformUtilService,
+            logService
+        );
+    }
+
+    async upsertOrganizationKey(organization: ProfileOrganizationResponse) {
+        const encOrgKeys = await this.localStorageService.get<any>(Keys.encOrgKeys);
+
+        encOrgKeys[organization.id] = organization.key;
+
+        await this.clearOrgKeys();
+        await this.localStorageService.save(Keys.encOrgKeys, encOrgKeys);
+    }
+
     protected async retrieveKeyFromStorage(keySuffix: KeySuffixOptions) {
         if (keySuffix === 'biometric') {
             await this.platformUtilService.authenticateBiometric();
