f8900211519734b4cd0a46191b83d00403974a9d
Théo Poizat <hello@zatteo.com>
2024-03-07 16:57:16 +0100

    feat: Hide "Custom fields" header and remove extra margin for Paper ciphers

--- apps/browser/src/vault/popup/components/vault/view-custom-fields.component.ts
+++ apps/browser/src/vault/popup/components/vault/view-custom-fields.component.ts
@@ -2,12 +2,19 @@ import { Component } from "@angular/core";
 
 import { ViewCustomFieldsComponent as BaseViewCustomFieldsComponent } from "@bitwarden/angular/vault/components/view-custom-fields.component";
 import { EventCollectionService } from "@bitwarden/common/abstractions/event/event-collection.service";
+/* Cozy customization */
+import { CipherType } from "@bitwarden/common/vault/enums/cipher-type";
+/* Cozy customization end */
 
 @Component({
   selector: "app-vault-view-custom-fields",
   templateUrl: "view-custom-fields.component.html",
 })
 export class ViewCustomFieldsComponent extends BaseViewCustomFieldsComponent {
+  /* Cozy customization */
+  cipherType = CipherType;
+  /* Cozy customization end */
+
   constructor(eventCollectionService: EventCollectionService) {
     super(eventCollectionService);
   }
