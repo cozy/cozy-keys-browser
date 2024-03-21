e59a0ea059bd6eccfe82d78beff74a80a23cc5ee
Théo Poizat <hello@zatteo.com>
2024-03-21 18:17:40 +0100

    feat: Add subtype and expirationData in Field
    For expiration date custom fields, we want to display an expiration
    annotation when the expiration date is expired or will expire soon.
    
    But we do not want to add the expiration annotation directly in the
    field value because this value is used
    in the autofill and we do not want to have the expiration annotation
    in the autofill.
    
    So we extend Field (and related classes) with :
    - a subtype
    - an expiration data object that contains everything we need to
    display the expiration annotation

--- libs/common/src/models/api/field.api.ts
+++ libs/common/src/models/api/field.api.ts
@@ -1,11 +1,17 @@
+import { FieldSubType } from "../../enums/fieldSubType";
 import { FieldType } from "../../enums/fieldType";
 import { LinkedIdType } from "../../enums/linkedIdType";
+import { ExpirationDateData } from "../../vault/models/data/expiration-date.data";
 import { BaseResponse } from "../response/base.response";
 
 export class FieldApi extends BaseResponse {
   name: string;
   value: string;
   type: FieldType;
+  // Cozy customization
+  subtype: FieldSubType;
+  expirationData: ExpirationDateData;
+  // Cozy customization end
   linkedId: LinkedIdType;
 
   constructor(data: any = null) {
@@ -14,6 +20,10 @@ export class FieldApi extends BaseResponse {
       return;
     }
     this.type = this.getResponseProperty("Type");
+    // Cozy customization
+    this.subtype = this.getResponseProperty("Subtype");
+    this.expirationData = this.getResponseProperty("ExpirationData");
+    // Cozy customization end
     this.name = this.getResponseProperty("Name");
     this.value = this.getResponseProperty("Value");
     this.linkedId = this.getResponseProperty("linkedId");
---
+++ libs/common/src/vault/models/data/expiration-date.data.ts
@@ -0,0 +1,7 @@
+// Cozy customization
+export type ExpirationDateData = {
+  isExpired: boolean;
+  isExpiringSoon: boolean;
+  expirationDate: string;
+};
+// Cozy customization end
