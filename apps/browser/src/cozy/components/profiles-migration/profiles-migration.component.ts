5e7ff03712f9692e9913b2220b7714ee2bee9269
Ldoppea <yannick.chiron@gmail.com>
2024-03-29 16:27:21 +0100

    feat: Compute and store deadline for `Profile` migration
    The first time the `Profile` migration Alert is displayed, then we want
    to start a delay of 15 days during which the user will be able to
    migrate their Profiles into Contacts
    
    This delay is stored in the browser's local storage into the form of a
    deadline and is not synced between clients
    
    The Alert will then display this deadline to the user

--- apps/browser/src/cozy/components/profiles-migration/profiles-migration.component.ts
+++ apps/browser/src/cozy/components/profiles-migration/profiles-migration.component.ts
@@ -1,4 +1,13 @@
 import { Component, Input, OnInit } from "@angular/core";
+/* eslint-disable import/no-duplicates */
+import addDays from "date-fns/addDays";
+import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
+import fr from "date-fns/locale/fr";
+
+import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
+import { StateService } from "@bitwarden/common/abstractions/state.service";
+
+const DELAY_IN_DAYS = 15;
 
 @Component({
   selector: "app-profiles-migration",
@@ -9,10 +18,26 @@ export class ProfilesMigrationComponent implements OnInit {
   remaining: string;
   deadline: string;
   ready = false;
+  constructor(
+    protected i18nService: I18nService,
+    protected stateService: StateService
+  ) {}
 
   async ngOnInit() {
-    this.remaining = "15 jours";
-    this.deadline = "15/04/2024";
+    let cleanDeadline = await this.stateService.getProfilesCleanDeadline();
+
+    if (!cleanDeadline) {
+      cleanDeadline = addDays(new Date(), DELAY_IN_DAYS);
+      this.stateService.setProfilesCleanDeadline(cleanDeadline);
+    }
+
+    this.deadline = cleanDeadline.toLocaleDateString();
+    this.remaining = formatDistanceToNowStrict(cleanDeadline, {
+      // @ts-expect-error I did not succeed in getting i18nService.translationLocale so I fallback to a private property
+      locale: this.i18nService.systemLanguage === "fr" ? fr : undefined,
+      unit: "day",
+    });
+
     this.ready = true;
   }
 
