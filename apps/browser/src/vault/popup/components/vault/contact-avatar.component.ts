daf97ed68008e1fc22876ac2ad7753ad32c5f766
Théo Poizat <hello@zatteo.com>
2024-04-17 11:50:46 +0200

    fix: Avatar color was not correct
    ContactAvatarComponent is displayed with a *cdkVirtualFor loop. As
    stated in the documentation, "To improve rendering performance,
    *cdkVirtualFor caches previously created views after they are no
    longer needed. When a new view would normally be created, a cached
    view is reused instead.".
    
    So we can not set the backgroundColor in ngOnInit because it will
    keep the background of the first elements created.

--- apps/browser/src/vault/popup/components/vault/contact-avatar.component.ts
+++ apps/browser/src/vault/popup/components/vault/contact-avatar.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { nameToColor } from "cozy-ui/transpiled/react/Avatar/helpers";
 
 @Component({
   selector: "app-vault-contact-avatar",
   templateUrl: "./contact-avatar.component.html",
 })
-export class ContactAvatarComponent {
+export class ContactAvatarComponent implements OnChanges {
   @Input() initials: string;
   @Input() size: number;
 
   backgroundColor: string;
 
-  async ngOnInit() {
+  ngOnChanges() {
     this.backgroundColor = nameToColor(this.initials);
   }
 }
