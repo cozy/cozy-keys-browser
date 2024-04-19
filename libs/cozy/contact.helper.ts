6b98ae715bbff48005acad7a561a5ea47660a096
Théo Poizat <hello@zatteo.com>
2024-04-19 11:10:48 +0200

    feat: Convert papers/contacts to CipherData instead of CipherResponse
    We were converting papers/contacts to CipherResponse because it is the model used by default during the sync.
    
    Here we choose to convert to CipherData instead because :
    - it will help us add a fallback to the sync. When papers/contacts sync will fail, we will be able to use the previous papers/contacts stored locally (they are stored as CipherData)
    - the conversion is simplier (it involves less classes)
    - we can still inject our papers/contacts during the sync thanks to the upsert method

--- libs/cozy/contact.helper.ts
+++ libs/cozy/contact.helper.ts
@@ -1,15 +1,14 @@
 import { models } from "cozy-client";
 
-import { ContactApi } from "@bitwarden/common/models/api/contact.api";
 import { SymmetricCryptoKey } from "@bitwarden/common/models/domain/symmetric-crypto-key";
 import { CipherType } from "@bitwarden/common/vault/enums/cipher-type";
-import { CipherResponse } from "@bitwarden/common/vault/models/response/cipher.response";
+import { CipherData } from "@bitwarden/common/vault/models/data/cipher.data";
 import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
 import { ContactView } from "@bitwarden/common/vault/models/view/contact.view";
 
 const { getInitials } = models.contact;
 
-import { buildFieldsFromContact, copyEncryptedFields } from "./fields.helper";
+import { buildFieldsFromContact } from "./fields.helper";
 
 const getPrimaryEmail = (contact: any): string | undefined => {
   return contact.email?.find((email: any) => email.primary)?.address;
@@ -19,12 +18,12 @@ const getPrimaryPhone = (contact: any): string | undefined => {
   return contact.phone?.find((phone: any) => phone.primary)?.number;
 };
 
-export const convertContactToCipherResponse = async (
+export const convertContactToCipherData = async (
   cipherService: any,
   i18nService: any,
   contact: any,
   key?: SymmetricCryptoKey
-): Promise<CipherResponse> => {
+): Promise<CipherData> => {
   const cipherView = new CipherView();
   cipherView.id = contact.id ?? contact._id;
   cipherView.name = contact.displayName;
@@ -36,25 +35,13 @@ export const convertContactToCipherResponse = async (
   cipherView.contact.primaryPhone = getPrimaryPhone(contact);
   cipherView.favorite = !!contact.cozyMetadata?.favorite;
   cipherView.fields = buildFieldsFromContact(i18nService, contact);
+  cipherView.contact.me = contact.me;
+  cipherView.creationDate = new Date(contact.cozyMetadata?.createdAt);
+  cipherView.revisionDate = new Date(contact.cozyMetadata?.updatedAt);
 
   const cipherEncrypted = await cipherService.encrypt(cipherView, key);
-  const cipherViewEncrypted = new CipherView(cipherEncrypted);
-  const cipherViewResponse = new CipherResponse(cipherViewEncrypted);
-  cipherViewResponse.id = cipherEncrypted.id;
-  cipherViewResponse.name = cipherEncrypted.name?.encryptedString ?? "";
-  cipherViewResponse.contact = new ContactApi();
-  cipherViewResponse.contact.displayName =
-    cipherEncrypted.contact.displayName?.encryptedString ?? "";
-  cipherViewResponse.contact.initials = cipherEncrypted.contact.initials?.encryptedString ?? "";
-  cipherViewResponse.contact.primaryEmail =
-    cipherEncrypted.contact.primaryEmail?.encryptedString ?? "";
-  cipherViewResponse.contact.primaryPhone =
-    cipherEncrypted.contact.primaryPhone?.encryptedString ?? "";
-  cipherViewResponse.favorite = cipherEncrypted.favorite;
-  cipherViewResponse.creationDate = contact.cozyMetadata?.createdAt;
-  cipherViewResponse.revisionDate = contact.cozyMetadata?.updatedAt;
-  cipherViewResponse.fields = copyEncryptedFields(cipherEncrypted.fields ?? []);
-  cipherViewResponse.contact.me = contact.me;
 
-  return cipherViewResponse;
+  const cipherData = cipherEncrypted.toCipherData();
+
+  return cipherData;
 };
