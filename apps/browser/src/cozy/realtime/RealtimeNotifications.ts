7b0cdacbf9a7c78e832691fea8c970d46cd49df6
Ldoppea <yannick.chiron@gmail.com>
2024-04-19 17:36:30 +0200

    feat: Add realtime for papers
    We want the extension to keep synchronized with the Cozy when a contact
    is created, edited or deleted from the mespapiers webapp
    
    This relies on the RealtimeNotifications class we created in the
    previous commit
    
    As the notification object does not contain all necessary fields (the
    structure is different and links are missing), we want to fetch the
    full Paper object before injecting it in the vault
    
    Also we don't want to show trashed papers, so when receiving a
    "trashed" update, we want to react as if it was a delete notification

--- apps/browser/src/cozy/realtime/RealtimeNotifications.ts
+++ apps/browser/src/cozy/realtime/RealtimeNotifications.ts
@@ -4,8 +4,16 @@ import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 import { MessagingService } from "@bitwarden/common/abstractions/messaging.service";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
 import { CipherData } from "@bitwarden/common/vault/models/data/cipher.data";
+import { CipherResponse } from "@bitwarden/common/vault/models/response/cipher.response";
 
-import { convertContactToCipherResponse } from "../../../../../libs/cozy/contact.helper"
+import { convertContactToCipherResponse } from "../../../../../libs/cozy/contact.helper";
+import {
+  convertNoteToCipherResponse,
+  fetchNoteIllustrationUrl,
+  isNote,
+} from "../../../../../libs/cozy/note.helper";
+import { convertPaperToCipherResponse } from "../../../../../libs/cozy/paper.helper";
+import { fetchPaper } from "../../../../../libs/cozy/queries";
 
 export class RealTimeNotifications {
   constructor(
@@ -32,6 +40,11 @@ export class RealTimeNotifications {
       this.dispatchCreateOrUpdateContact.bind(this)
     );
     await realtime.subscribe("deleted", doctypeContact, this.dispatchDeleteCipher.bind(this));
+
+    const doctypePaper = "io.cozy.files";
+    // We don't want to listen Creation as it is always followed by an Update notification with more data
+    await realtime.subscribe("updated", doctypePaper, this.dispatchUpdatePaper.bind(this));
+    await realtime.subscribe("deleted", doctypePaper, this.dispatchDeleteCipher.bind(this));
   }
 
   async unregister() {
@@ -43,13 +56,18 @@ export class RealTimeNotifications {
     await realtime.unsubscribe("created", doctypeContact, this.dispatchCreateOrUpdateContact);
     await realtime.unsubscribe("updated", doctypeContact, this.dispatchCreateOrUpdateContact);
     await realtime.unsubscribe("deleted", doctypeContact, this.dispatchDeleteCipher);
+
+    const doctypePaper = "io.cozy.files";
+    await realtime.unsubscribe("updated", doctypePaper, this.dispatchUpdatePaper);
+    await realtime.unsubscribe("deleted", doctypePaper, this.dispatchDeleteCipher);
   }
 
   async dispatchCreateOrUpdateContact(data: any) {
     const cipherResponse = await convertContactToCipherResponse(
       this.cipherService,
       this.i18nService,
-      data
+      data,
+      null
     );
     await this.cipherService.upsert(new CipherData(cipherResponse));
     this.messagingService.send("syncedUpsertedCipher", { cipherId: data._id });
@@ -59,6 +77,57 @@ export class RealTimeNotifications {
   async dispatchDeleteCipher(data: any) {
     await this.cipherService.delete(data._id);
     this.messagingService.send("syncedDeletedCipher", { cipherId: data._id });
+    this.messagingService.send("syncCompleted", { successfully: true });
+  }
+
+  async dispatchUpdatePaper(data: any) {
+    if (data.type !== "file") {
+      return;
+    }
+    if (!data.metadata?.qualification?.label) {
+      return;
+    }
+    if (data.dir_id === "io.cozy.files.trash-dir") {
+      // We don't want to display trashed papers in the extension's bin so we remove them from the vault
+      return this.dispatchDeleteCipher(data);
+    }
+
+    await this.upsertPaperFromId(data._id);
+  }
+
+  async upsertPaperFromId(paperId: string) {
+    const itemFromDb = await fetchPaper(this.client, paperId);
+    const hydratedData = this.client.hydrateDocuments("io.cozy.files", [itemFromDb])[0];
+
+    let cipherResponse: CipherResponse;
+    if (isNote(itemFromDb)) {
+      const noteIllustrationUrl = await fetchNoteIllustrationUrl(this.client);
+
+      cipherResponse = await convertNoteToCipherResponse(
+        this.cipherService,
+        this.i18nService,
+        hydratedData,
+        null,
+        {
+          noteIllustrationUrl,
+        }
+      );
+    } else {
+      const baseUrl = this.client.getStackClient().uri;
+
+      cipherResponse = await convertPaperToCipherResponse(
+        this.cipherService,
+        this.i18nService,
+        hydratedData,
+        null,
+        {
+          baseUrl,
+        }
+      );
+    }
+
+    await this.cipherService.upsert(new CipherData(cipherResponse));
+    this.messagingService.send("syncedUpsertedCipher", { cipherId: paperId });
     this.messagingService.send("syncCompleted", { successfully: true });
   }
 }
