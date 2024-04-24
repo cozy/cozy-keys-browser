0fd90571c303a63eaab1a2c50ba475f80dde5bf4
Théo Poizat <hello@zatteo.com>
2024-04-24 18:52:26 +0200

    feat: Use expiration date messages from cozy-client

--- apps/browser/src/vault/popup/components/vault/view-expiration-date.component.ts
+++ apps/browser/src/vault/popup/components/vault/view-expiration-date.component.ts
@@ -1,25 +1,42 @@
-/* eslint-disable import/no-duplicates */
-import { Component, Input } from "@angular/core";
-import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
-import fr from "date-fns/locale/fr";
+import { Component, Input, OnInit } from "@angular/core";
+import { models } from "cozy-client";
 
 import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 import { FieldView } from "@bitwarden/common/vault/models/view/field.view";
+
+const { makeExpiredMessage, makeExpiresInMessage } = models.paper;
 
 @Component({
   selector: "app-vault-view-expiration-date",
   templateUrl: "./view-expiration-date.component.html",
 })
-export class ViewExpirationDateComponent {
+export class ViewExpirationDateComponent implements OnInit {
   @Input() field: FieldView;
+  message: string;
 
   constructor(private i18nService: I18nService) {}
 
-  getExpirationDistance(): string {
-    return formatDistanceToNowStrict(
-      new Date(this.field.expirationData.expirationDate),
+  ngOnInit() {
+    if (this.field.expirationData.isExpired) {
+      this.message = this.getExpiredMessage();
+    } else if (this.field.expirationData.isExpiringSoon) {
+      this.message = this.getExpiresInMessage();
+    }
+  }
+
+  private getExpiredMessage(): string {
+    return makeExpiredMessage(
+      this.field.expirationData.expirationDate,
       // @ts-expect-error I did not succeed in getting i18nService.translationLocale so I fallback to a private property
-      { locale: this.i18nService.systemLanguage === "fr" ? fr : undefined }
+      { lang: this.i18nService.systemLanguage }
+    );
+  }
+
+  private getExpiresInMessage(): string {
+    return makeExpiresInMessage(
+      this.field.expirationData.expirationDate,
+      // @ts-expect-error I did not succeed in getting i18nService.translationLocale so I fallback to a private property
+      { lang: this.i18nService.systemLanguage }
     );
   }
 }
