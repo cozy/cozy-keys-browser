073ba94fada51dd4b0f650afab481d5b37a045a6
Théo Poizat <hello@zatteo.com>
2024-06-26 12:14:40 +0200

    fix(2024.5.1): Mock ConfigApiService call

--- libs/common/src/platform/services/config/config-api.service.ts
+++ libs/common/src/platform/services/config/config-api.service.ts
@@ -13,10 +13,19 @@ export class ConfigApiService implements ConfigApiServiceAbstraction {
   async get(userId: UserId | undefined): Promise<ServerConfigResponse> {
     // Authentication adds extra context to config responses, if the user has an access token, we want to use it
     // We don't particularly care about ensuring the token is valid and not expired, just that it exists
+
+    // Cozy customization, mock ConfigApi server response
+    // cozy-stack does not implement /api/config route
+    // These route is used mainly to get server configuration and feature flags
+    //*
+    return new ServerConfigResponse(null);
+    /*/
     const authed: boolean =
       userId == null ? false : (await this.tokenService.getAccessToken(userId)) != null;
 
     const r = await this.apiService.send("GET", "/config", null, authed, true);
     return new ServerConfigResponse(r);
+    //*/
+
   }
 }
