4e661a6c1f8eda4fc3af60a1dbfca4e530ece4ee
AlexisG <merkkur39@gmail.com>
2024-10-04 17:18:01 +0200

    refactor: Remove useless imports and prop

--- apps/browser/src/autofill/overlay/inline-menu/pages/list/autofill-inline-menu-list.ts
+++ apps/browser/src/autofill/overlay/inline-menu/pages/list/autofill-inline-menu-list.ts
@@ -53,17 +53,14 @@ import {
 } from "../../../../../autofill/types";
 import {
   COZY_ATTRIBUTES_MAPPING,
-  CozyContactFieldNames,
   cozypaperFieldNames,
 } from "../../../../../../../../libs/cozy/mapping";
 import { CozyAutofillOptions } from "src/autofill/services/abstractions/autofill.service";
-import { fields } from "../../../../../../../../libs/cozy/contact.lib";
 /* eslint-enable */
 /* end Cozy imports */
 
 export class AutofillInlineMenuList extends AutofillInlineMenuPageElement {
   private inlineMenuListContainer: HTMLDivElement;
-  private actionMenuContainer: HTMLDivElement;
   private resizeObserver: ResizeObserver;
   private eventHandlersMemo: { [key: string]: EventListener } = {};
   private ciphers: InlineMenuCipherData[] = [];
